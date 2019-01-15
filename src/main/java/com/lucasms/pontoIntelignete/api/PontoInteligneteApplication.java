421af82da1b17ff8fd84e628222e4a800212ce79
Fernandes <lucas.dos.fernandes@accenture.com>
2019-01-15 10:57:21 -0200

    Initial commit

---
+++ src/main/java/com/lucasms/pontoIntelignete/api/PontoInteligneteApplication.java
@@ -0,0 +1,14 @@
+package com.lucasms.pontoIntelignete.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligneteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligneteApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/lucasms/pontoIntelignete/api/PontoInteligneteApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lucasms.pontoIntelignete.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PontoInteligneteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
